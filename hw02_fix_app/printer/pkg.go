cfde06f5e3e84068b1e5608471e748f6d8cb872c
ildarmukhametzyanov <ildarmukhametzyanov@yahoo.com>
2024-01-25 21:21:50 +0300

    HW1 is completed

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -9,7 +9,7 @@ import (
 func PrintStaff(staff []types.Employee) {
 	var str string
 	for i := 0; i < len(staff); i++ {
-		str = fmt.Sprintf("UserID: %s; Age: %d; Name: %s; DepartmentID: %d", staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
+		str = fmt.Sprintf("UserID: %d; Age: %d; Name: %s; DepartmentID: %d", staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
 		fmt.Println(i)
 	}
 
