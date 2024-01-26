d498117a4caa0f7c51ac860509419a0429c7cc4d
ildarmukhametzyanov <ildarmukhametzyanov@yahoo.com>
2024-01-26 10:46:18 +0300

    HW1 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,15 +1,15 @@
-package init
+package main
 
 import (
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
 	"fmt"
+
+	"github.com/ildar52/hw-test/printer"
+	"github.com/ildar52/hw-test/reader"
+	"github.com/ildar52/hw-test/types"
 )
 
-func init() {
-	var path string = "data.json"
-
+func main() {
+	path := "data.json"
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
@@ -18,10 +18,9 @@ func init() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	staff, err = reader.ReadJSON(path)
 
 	fmt.Print(err)
 
